449c5203982eed025b9afb30efc50a93d16006b8
Anastasiia.Aheienko.PM.2022@lpnu.ua <Anastasiia.Aheienko.PM.2022@lpnu.ua>
2023-12-03 23:28:28 +0200

    new index

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,29 +1,58 @@
 window.onload = (event) => {
-  const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
+  // Doughnut Chart
+  const doughnutData = {
+      labels: ['Red', 'Blue', 'Yellow'],
+      datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+      }]
   };
 
-  const config = {
-    type: 'doughnut',
-    data: data
+  const doughnutConfig = {
+      type: 'doughnut',
+      data: doughnutData
   };
 
-  let myChart = document.getElementById('donought').getContext('2d');
+  let doughnutChart = document.getElementById('doughnutChart').getContext('2d');
+  new Chart(doughnutChart, doughnutConfig);
 
-  new Chart(myChart, config);
+  // Area Chart
+  const areaData = {
+      labels: ["January", "February", "March", "April", "May"],
+      datasets: [{
+          label: "Monthly Sales",
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)",
+          borderWidth: 1,
+          data: [65, 59, 80, 81, 56]
+      }]
+  };
+
+  const areaOptions = {
+      scales: {
+          x: [{
+              type: 'category',
+              labels: areaData.labels
+          }],
+          y: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }
+  };
+
+  let areaChart = document.getElementById('myAreaChart').getContext('2d');
+  new Chart(areaChart, {
+      type: 'line',
+      data: areaData,
+      options: areaOptions
+  });
 }
 
