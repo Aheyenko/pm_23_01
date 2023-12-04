bc4303839955e1c4f8f39dc902904164d0e02427
Anastasiia.Aheienko.PM.2022@lpnu.ua <Anastasiia.Aheienko.PM.2022@lpnu.ua>
2023-12-04 04:22:20 +0200

    green and purple graphs

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -3,14 +3,14 @@ window.onload = function () {
     const initialHoverOffset = 4;
 
     const doughnutData = {
-        labels: ['Red', 'Blue', 'Yellow'],
+        labels: [],
         datasets: [{
-            label: 'My First Dataset',
+            label: [],
             data: [300, 50, 100],
             backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
+                '#ffc400',
+                '#38a4dd',
+                '#b39ddb'
             ],
             hoverOffset: initialHoverOffset
         }]
@@ -27,7 +27,7 @@ display:false,
                 const index = clickedElement.index;
 
                 // Move the clicked piece to the side (e.g., increase the hoverOffset)
-                doughnutConfig.data.datasets[datasetIndex].hoverOffset = 8;
+                doughnutConfig.data.datasets[datasetIndex].hoverOffset = 30;
 
                 // Redraw the chart
                 doughnutChart.update();
@@ -43,7 +43,7 @@ display:false,
 
     let doughnutChart = new Chart(document.getElementById('doughnutChart').getContext('2d'), doughnutConfig);
 
-    // Area Chart
+    // Area  Chart
     let wavyGraph = document.getElementById('wavesGraph').getContext('2d');
 
   const gradient = wavyGraph.createLinearGradient(0, 0, 0, 400);
@@ -98,4 +98,175 @@ display:false,
     },
     }
   });
+  //Purple graphs
+
+  const labels = ['1', '2', '3', '4', '5', '6', '7','8'];
+  const data = [
+      {
+          label: 'My First dataset',
+          data: ['50','100','120','40','35', '85', '100','90'],
+          borderColor: '#b198dc',
+          pointBackgroundColor: 'rgba(0,0,0,0)',
+          pointBorderWidth:0,
+          pointHoverBorderWidth:0,
+          backgroundColor: '#b198dc',
+          pointHoverBackgroundColor: '#b198dc',
+          pointHoverRadius: 8, 
+          pointHoverBorderColor:' #e7e0f4',     // Point radius on hover
+          pointHoverBorderWidth:3,
+          tension: 0.4,
+          fill: true,
+      },
+      {
+          label: 'My Second dataset',
+          data: ['105','115','90','80','115','110', '85','120'],
+          borderColor: '#e7e0f4',
+          pointBackgroundColor: 'rgba(0,0,0,0)',
+          pointBorderWidth:0,
+          pointHoverBorderWidth:0,
+          backgroundColor: '#e7e0f4',
+          pointRadius: 5,
+    
+          tension: 0.4,
+          fill: true,
+      },
+  ];
+
+  const config = {
+      type: 'line',
+      data:{
+        labels: labels,
+        datasets: data,
+      },
+      options: {
+          responsive: true,
+          plugins: {
+              title: {
+                  display: false,
+
+              },
+              tooltip: {
+                  enabled: false,
+              },
+              legend: {
+                display: false,
+            },
+          },
+          interaction: {
+              mode: 'nearest',
+              axis: 'x',
+              intersect: false
+          },
+          scales: {
+              x: {
+                  title: {
+                      display: false,
+                  },
+                  display: false,
+              },
+              y: {
+                  stacked: true,
+                  title: {
+                      display: false,
+                  },
+                  display: false,
+                  min:0,
+                  max:300,
+              }
+          },
+          maintainAspectRatio: false,
+          tooltips: {
+            enabled: false // Enable tooltips to display data on hover
+          },
+
+      }
+  };
+
+  // Initialize the chart with the provided data and options
+  let purpleGraph = new Chart(document.getElementById('purpleGraph').getContext('2d'), config);
+   //Green graphs
+
+   const labels1 = ['1', '2', '3', '4', '5', '6', '7','8'];
+   const data1 = [
+       {
+           label: 'My First dataset',
+           data: ['95','100','120','40','35', '85', '100','90'],
+           borderColor: '#cdedea',
+           pointBackgroundColor: 'rgba(0,0,0,0)',
+           pointBorderWidth:0,
+           pointHoverBorderWidth:0,
+           backgroundColor: '#cdedea',
+           pointHoverBackgroundColor: '#6dc7be',
+           pointHoverRadius: 8, 
+           pointHoverBorderColor:'#cdedea',     // Point radius on hover
+           pointHoverBorderWidth:3,
+           tension: 0.4,
+           fill: true,
+       },
+       {
+           label: 'My Second dataset',
+           data: ['200','115','90','80','115','110', '85','120'],
+           borderColor: '#6dc7be',
+           pointBackgroundColor: 'rgba(0,0,0,0)',
+           pointBorderWidth:0,
+           pointHoverBorderWidth:0,
+           backgroundColor: '#6dc7be',
+        
+           tension: 0.4,
+           fill: true,
+       },
+   ];
+ 
+   const config1 = {
+       type: 'line',
+       data:{
+         labels: labels1,
+         datasets: data1,
+       },
+       options: {
+           responsive: true,
+           plugins: {
+               title: {
+                   display: false,
+ 
+               },
+               tooltip: {
+                   enabled: false,
+               },
+               legend: {
+                 display: false,
+             },
+           },
+           interaction: {
+               mode: 'nearest',
+               axis: 'x',
+               intersect: false
+           },
+           scales: {
+               x: {
+                   title: {
+                       display: false,
+                   },
+                   display: false,
+               },
+               y: {
+                   stacked: true,
+                   title: {
+                       display: false,
+                   },
+                   display: false,
+                   min:0,
+                   max:300,
+               }
+           },
+           maintainAspectRatio: false,
+           tooltips: {
+             enabled: false // Enable tooltips to display data on hover
+           },
+ 
+       }
+   };
+ 
+   // Initialize the chart with the provided data and options
+   let greenGraph = new Chart(document.getElementById('greenGraph').getContext('2d'), config1);
 }
